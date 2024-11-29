9e92b7364d9de8d6b958c2c5e9b4d27a4e9ee1f2
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2024-11-29 17:31:34 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,14 +1,23 @@
-from math import *
+# from math import *
+
+# def solution(number):
+#     n = len(number) # 학생 수
+#     total_case = (factorial(n) / (factorial(3) * factorial(n-3))) # 3명을 뽑는 전체 케이스의 수 
+    
+#     answer = 0
+#     for i in range(len(number)):
+#         for j in range(i + 1, len(number)):
+#             for k in range(j + 1, len(number)):
+#                 if number[i] + number[j] + number[k] == 0:
+#                     answer += 1
+                
+#     return answer
+
+from itertools import combinations
 
 def solution(number):
-    n = len(number) # 학생 수
-    total_case = (factorial(n) / (factorial(3) * factorial(n-3))) # 3명을 뽑는 전체 케이스의 수 
-    
     answer = 0
-    for i in range(len(number)):
-        for j in range(i + 1, len(number)):
-            for k in range(j + 1, len(number)):
-                if number[i] + number[j] + number[k] == 0:
-                    answer += 1
-                
+    for i in combinations(number, 3):
+        if sum(i) == 0:
+            answer += 1
     return answer
