30277b8b8bbfb1dcc5d202912a7f58628bfc399c
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2024-11-29 17:02:45 +0900

    [level 1] Title: 삼총사, Time: 0.11 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,14 @@
+from math import *
+
+def solution(number):
+    n = len(number) # 학생 수
+    total_case = (factorial(n) / (factorial(3) * factorial(n-3))) # 3명을 뽑는 전체 케이스의 수 
+    
+    answer = 0
+    for i in range(len(number)):
+        for j in range(i + 1, len(number)):
+            for k in range(j + 1, len(number)):
+                if number[i] + number[j] + number[k] == 0:
+                    answer += 1
+                
+    return answer
