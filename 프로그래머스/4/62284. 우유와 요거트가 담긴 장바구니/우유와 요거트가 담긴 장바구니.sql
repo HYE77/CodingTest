76183d0b9de74bff7e6d4ec84a813a53e51489d3
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2024-12-02 19:11:38 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -1,7 +1,14 @@
--- 코드를 입력하세요
-SELECT MILK.CART_ID
-FROM (SELECT CART_ID
-    FROM CART_PRODUCTS
-    WHERE NAME = 'Milk')  MILK INNER JOIN (SELECT CART_ID
-                                      FROM CART_PRODUCTS
-                                      WHERE NAME = 'Yogurt') YOGURT ON MILK.CART_ID = YOGURT.CART_ID
+
+# SELECT MILK.CART_ID
+# FROM (SELECT CART_ID
+#     FROM CART_PRODUCTS
+#     WHERE NAME = 'Milk')  MILK INNER JOIN (SELECT CART_ID
+#                                       FROM CART_PRODUCTS
+#                                       WHERE NAME = 'Yogurt') YOGURT ON MILK.CART_ID = YOGURT.CART_ID
+
+
+SELECT CART_ID
+FROM CART_PRODUCTS
+GROUP BY CART_ID
+HAVING GROUP_CONCAT(NAME) LIKE '%Milk%' AND GROUP_CONCAT(NAME) LIKE '%Yogurt%'
+ORDER BY 1
