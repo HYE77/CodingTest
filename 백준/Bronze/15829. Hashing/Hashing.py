a4c7594504f21c3c7c59b7284802fe3f927731bf
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-05-28 17:02:55 +0900

    [Bronze II] Title: Hashing, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -3,5 +3,5 @@ list = list(input())
 # ord(_) - 96
 ans = 0
 for i in range(L):
-    ans += (ord(list[i]) - 96) * (31 ** i)
-print(ans)
+    ans += ((ord(list[i]) - 96) * (31 ** i))
+print(ans % 1234567891)
