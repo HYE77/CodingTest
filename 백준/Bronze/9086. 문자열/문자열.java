c7defa04afc260bf08eab0c0b54ddde852b2f4d1
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-08-05 14:53:31 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			System.out.println(Character.toString(str.charAt(0)) + Character.toString(str.charAt(str.length()-1)));
+			
+		}
+		
+
+
+	}
+
+}
