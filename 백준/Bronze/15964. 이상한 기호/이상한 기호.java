7f1bdac9b194d6ec117974c7e5a5f2317b9708dc
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-08-05 15:07:44 +0900

    [Bronze V] Title: 이상한 기호, Time: 208 ms, Memory: 17660 KB, Score: 30 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println(cal(A, B));
+		
+		sc.close();
+
+	}
+	
+	public static int cal(int A, int B) {
+		int ans = (A+B)*(A-B);
+		return ans;
+	}
+
+}
