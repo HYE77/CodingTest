0217f48f83ca9d5fcbd953853673779f7d15f5ab
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-08-12 08:59:10 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        
+        System.out.println(id + "??!");
+    }
+}
