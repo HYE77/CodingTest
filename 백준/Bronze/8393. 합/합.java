c4aca729efa06b73f0172cb656bb608e1666a71b
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-08-12 13:37:34 +0900

    [Bronze V] Title: 합, Time: 168 ms, Memory: 17516 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		int N = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+		
+		sc.close();
+ 	}
+
+}
