1f37b5d1d0330a12745070dd9357902145a80fc3
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-08-20 17:54:58 +0900

    [Bronze V] Title: 스위트콘 가격 구하기, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/30030. 스위트콘 가격 구하기/스위트콘 가격 구하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int B = Integer.parseInt(br.readLine());
+		
+		int ans = B * 10 / 11;
+		
+		bw.write(ans+"");
+		bw.flush();
+		br.close();
+		bw.close();
+
+	}
+
+}
