81386e620839ec537f6ed70a9d99fb32e3f183b5
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-08-30 16:50:58 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 88 ms, Memory: 25,216 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,52 +1,44 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		for (int t = 1; t <= 10; t++) {
-			int len = Integer.parseInt(br.readLine()); // length of string
-			String str = br.readLine();
-			
-			char[] arr = str.toCharArray();
-			List<Character> opens = new ArrayList<>(Arrays.asList('(', '[', '<', '{'));
-			List<Character> closes = new ArrayList<>(Arrays.asList(')', ']', '>', '}'));
-			
-			Stack<Character> stack = new Stack<>();
-			boolean isOk = true;
-			
-			for (int i = 0; i < len; i++) {
-				char temp = arr[i];
-				if (opens.contains(temp)) {// 여는 괄호라면
-					stack.push(temp);
-				} else { // 닫는 괄호라면
-					if (stack.isEmpty()) { // 스택이 비었다면 -> 이슈
-						isOk = false;
-						break;
-					} else { // pop해서 비교해보자
-						char last = stack.peek();
-						if (opens.indexOf(last) == closes.indexOf(temp)) stack.pop(); // 짝이 맞으면 pop
-						else { // 짝이 다르면 break
-							isOk = false;
-							break;
-						}
-					}
-				}
-			}
-			int ans;
-			if (isOk = true && stack.isEmpty()) ans = 1;
-			else ans = 0;
-			
-			System.out.println("#" + t + " " + ans);
-		}
+    static List<Character> open = new ArrayList<>(Arrays.asList('(', '{', '<', '['));
+    static List<Character> close = new ArrayList<>(Arrays.asList(')', '}', '>', ']'));
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int t = 1; t <= 10; t++) {
+            int N = Integer.parseInt(br.readLine());
+            String input = br.readLine();
+            char[] arr = input.toCharArray();
+
+            Stack<Character> stack = new Stack<>();
+
+            boolean isOk = true;
+            for (char c : arr) {
+                if (open.contains(c)) {
+                    // 여는 괄호라면
+                    stack.push(c);
+                } else {
+                    // 닫는 괄활면
+                    char tmp = stack.pop();
+                    if (open.indexOf(tmp) != close.indexOf(c)) {
+                        isOk = false;
+                        break;
+                    }
+                }
+            }
+
+            if (stack.isEmpty() && isOk)
+                System.out.println("#" + t + " 1" );
+            else System.out.println("#" + t + " 0");
+
+        }
+
+        br.close();
+
+    }
 
 }
