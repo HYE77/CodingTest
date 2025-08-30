7443865c459d877740839e22f280c6d845f98ac6
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-08-30 16:31:00 +0900

    [Bronze IV] Title: Quick Estimates, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+            String num = br.readLine();
+            System.out.println(num.length());
+
+        }
+
+        br.close();
+
+    }
+
+}
