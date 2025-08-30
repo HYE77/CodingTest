073edfdc0e835cc878a53c04877e8e5e3bd78cab
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-08-30 16:34:54 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= N; i++ ) {
+            for (int s = 0; s < i; s++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        for (int i = N-1; i > 0; i--) {
+            for (int s = 0; s < i; s++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+
+
+        br.close();
+
+    }
+
+}
