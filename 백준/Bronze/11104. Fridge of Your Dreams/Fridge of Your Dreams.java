cce93624770008fdc226a799ab33a25ca0a7528f
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-09-04 10:12:49 +0900

    [Bronze II] Title: Fridge of Your Dreams, Time: 136 ms, Memory: 14964 KB -BaekjoonHub

---
+++ 백준/Bronze/11104. Fridge of Your Dreams/Fridge of Your Dreams.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 0; t < T; t++) {
+			System.out.println(Integer.parseInt(br.readLine(), 2));
+		}
+		
+		br.close();
+	}
+}	
