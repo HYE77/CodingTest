735c9e8a8f7f8c781565ba1fb47a924b6044591a
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-09-09 20:23:58 +0900

    [Silver II] Title: 최소 힙, Time: 312 ms, Memory: 26768 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine()); // 입력 개수
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            int tmp = Integer.parseInt(br.readLine());
+            if (tmp == 0 && pq.isEmpty()) bw.write("0\n");
+            else if (tmp == 0 && !pq.isEmpty()) bw.write(pq.poll()+"\n");
+            else pq.add(tmp);
+        }
+
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+
+}
