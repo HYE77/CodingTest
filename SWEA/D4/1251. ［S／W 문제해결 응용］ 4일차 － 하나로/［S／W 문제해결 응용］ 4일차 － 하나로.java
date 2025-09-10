124050ffc421976f00198c3f056735ff5eb49eb0
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-09-10 15:00:50 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 439 ms, Memory: 88,348 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,144 +1,150 @@
 import java.util.*;
 import java.io.*;
-
+ 
 public class Solution {
-	
-	static int N;
-	static Island[] islands;
-	static ArrayList<Edge> edges;
-	static Island[] selected;
-	static int[] p;
-	
- 	static class Island {
-		int idx, x, y; // 섬 번호, x좌표, y좌표
-		
-		public Island(int idx, int x) {
-			this.idx = idx;
-			this.x = x;
-		}
+     
+    static int N;
+    static Island[] islands;
+    static List<Edge>[] adj;
+    static boolean[] visited;
+    static long[] dist;
+    static long answer;
+     
+    static class Island {
+        int idx, x, y; // 섬 번호, x좌표, y좌표
+         
+        public Island(int idx, int x) {
+            this.idx = idx;
+            this.x = x;
+        }
+ 
+        public Island(int idx, int x, int y) {
+            this.idx = idx;
+            this.x = x;
+            this.y = y;
+        }
+    }
+     
+    static class Edge implements Comparable<Edge> {
+        int from, to;
+        long dist; // 출발섬, 도착섬, 거리 제곱
+ 
+        public Edge(int from, int to, long dist) {
+            this.from = from;
+            this.to = to;
+            this.dist = dist;
+        }
+        
+        public Edge(int to, long dist) {
+            this.to = to;
+            this.dist = dist;
+        }
+ 
+        @Override
+        public int compareTo(Edge o) {
+            if (this.dist < o.dist) return -1;
+            else return 1;
+        }
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+         
+        int T = Integer.parseInt(br.readLine());
+         
+        for (int t = 1; t <= T; t++) {
+             
+            N = Integer.parseInt(br.readLine()); // 섬의 개수
+            islands = new Island[N]; // 섬 객체 배열 선언
+            visited = new boolean[N];
+             
+            // 섬 배열 만들기
+            // 1) 섬 인덱스와 x좌표 저장
+            st = new StringTokenizer(br.readLine()); // 섬들의 x좌표
+            for (int n = 0; n < N; n++) {
+                islands[n] = new Island(n, Integer.parseInt(st.nextToken()));
+            }
+             
+            // 2) y좌표 추가
+            st = new StringTokenizer(br.readLine()); // 섬들의 y좌표
+            for (int n = 0; n < N; n++) {
+                islands[n].y =Integer.parseInt(st.nextToken());
+            }
+             
+            double E = Double.parseDouble(br.readLine()); // 세율
 
-		public Island(int idx, int x, int y) {
-			this.idx = idx;
-			this.x = x;
-			this.y = y;
-		}
-	}
-	
-	static class Edge implements Comparable<Edge> {
-		int from, to;
-		long dist; // 출발섬, 도착섬, 거리 제곱
+            
+            // 섬 리스트에서 2개씩 뽑아서 edge 정보 adj에 추가하기
+            adj = new ArrayList[N];
+            for (int i = 0; i < N; i++) {
+    			adj[i] = new ArrayList<>();
+    		} // 초기화 완료
+            
+            for (int i = 0; i < N; i++) {
+            	for (int j = i+1; j < N; j++) {
+            		Island a = islands[i];
+            		Island b = islands[j];
+            		long distance = (long) (Math.pow(a.x-b.x, 2) +  Math.pow(a.y-b.y, 2));
+            		
+            		adj[a.idx].add(new Edge(b.idx, distance)); // to, distance
+            		adj[b.idx].add(new Edge(a.idx, distance)); // to, distance
+            		
+            	}
+            }
+             
+           
+            // MST 만들기
+            dist = new long[N];
+            Arrays.fill(dist, Long.MAX_VALUE);
+            long answer = 0l;
+            
+            dijkstra(0);
+            
+            for (long d : dist) {
+            	answer += d;
+            }
+            
+            answer = Math.round(answer * E);
+            bw.write("#" + t + " " + answer + "\n");
+             
+             
+        }
+ 
+         
+        bw.flush();
+        br.close();
+        bw.close();
+         
+    }
+     
+    static void dijkstra(int start) {
+    	
+    	PriorityQueue<Edge> pq = new PriorityQueue<>();
+    	
+    	dist[0] = 0;
+    	
+    	pq.add(new Edge(start, 0));
+    	
+    	while (!pq.isEmpty()) {
+    		Edge curr = pq.poll(); // 제일 가까운 애 뽑았다
+    		
+    		if (visited[curr.to]) continue;
+    		visited[curr.to] = true; // 방문처리
+    		
+    		// 얘의 이웃들 중 갱신할 거 있으면 하자
+    		for (Edge e : adj[curr.to]) {
+    			if (!visited[e.to] && dist[e.to] > e.dist ) {
+    				dist[e.to] =   e.dist;
+    				pq.add(new Edge(e.to, dist[e.to]));
+    			}
+    		}
+    			
+    	}	
+    	
+    }
 
-		public Edge(int from, int to, long dist) {
-			this.from = from;
-			this.to = to;
-			this.dist = dist;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			if (this.dist < o.dist) return -1;
-			else return 1;
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int t = 1; t <= T; t++) {
-			
-			N = Integer.parseInt(br.readLine()); // 섬의 개수
-			islands = new Island[N]; // 섬 객체 배열 선언
-			
-			// 서로소집합 초기화
-			p = new int[N];
-			for (int i = 0; i < N; i++) {
-				p[i] = i;
-			}
-			
-			// 섬 배열 만들기
-			// 1) 섬 인덱스와 x좌표 저장
-			st = new StringTokenizer(br.readLine()); // 섬들의 x좌표
-			for (int n = 0; n < N; n++) {
-				islands[n] = new Island(n, Integer.parseInt(st.nextToken()));
-			}
-			
-			// 2) y좌표 추가
-			st = new StringTokenizer(br.readLine()); // 섬들의 y좌표
-			for (int n = 0; n < N; n++) {
-				islands[n].y =Integer.parseInt(st.nextToken());
-			}
-			
-			double E = Double.parseDouble(br.readLine()); // 세율
-			
-			// edge 리스트 만들기
-			edges = new ArrayList<>();
-			selected = new Island[2];
-			findAllEdges(0, 0);
-			
-			// edge 리스트 정렬
-			Collections.sort(edges);
-			
-			// Kruskal 알고리즘으로 MST 탐색
-			int edgeCnt = 0;
-			int edgeIdx = 0;
-			long answer = 0l;
-			while (edgeCnt < N-1 && edgeIdx < edges.size()) {
-				Edge tmp = edges.get(edgeIdx);
-				int a = tmp.from;
-				int b = tmp.to;
-				
-				int pa = findSet(a);
-				int pb = findSet(b);
-				
-				if (pa != pb) {
-					p[pa] = pb;
-					answer += tmp.dist;
-					edgeCnt++;
-				}
-				
-				edgeIdx++;
-				
-			}
-			answer = Math.round(answer * E);
-			bw.write("#" + t + " " + answer + "\n");
-			
-			
-		}
-
-		
-		bw.flush();
-		br.close();
-		bw.close();
-		
-	}
-	
-	static void findAllEdges(int sidx, int idx) {
-		// 조합 완성했으면
-		if (sidx == 2) {
-			Island a = selected[0];
-			Island b = selected[1];
-			long distance = (long) (Math.pow(a.x-b.x, 2) +  Math.pow(a.y-b.y, 2));
-			
-			edges.add(new Edge(a.idx, b.idx, distance));
-			return;
-		}
-		
-		for (int i = idx; i < N; i++) {
-			selected[sidx] = islands[i];
-			findAllEdges(sidx+1, i+1);
-		}
-		
-	}
-	
-	
-	static int findSet(int v) {
-		if (p[v] != v) p[v] = findSet(p[v]);
-		return p[v]; 
-	}
-	
-
+     
+ 
 }
