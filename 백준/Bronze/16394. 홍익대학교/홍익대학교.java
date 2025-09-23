b800f2d5caa8c45b9e9b059c1314211e45818d17
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-09-24 08:50:16 +0900

    [Bronze V] Title: 홍익대학교, Time: 168 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		int open = 1946;
+		System.out.println(input - open);
+	
+	}
+
+}
