39dd2f9e71fdf5c1f98932cc97d28fd73e27df4c
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-09-24 16:28:31 +0900

    [Silver I] Title: IOIOI, Time: 248 ms, Memory: 20200 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -2,31 +2,33 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        String S = br.readLine();
 
-		int N = Integer.parseInt(br.readLine());
-		int M = Integer.parseInt(br.readLine());
-		String S = br.readLine();
-		
-		String IO = "";
-		for (int i = 0; i < N; i++) {
-			IO += "IO";
-		}
-		IO += "I";
-		
-		int ans = 0;
-		int idx = 0;
-		while (idx < M-(2*N+1)+1) {
-			if (S.substring(idx, idx+IO.length()).equals(IO)) ans++;
-			idx++;
-		}
-		
-		bw.write(ans+"");
-		bw.flush();
-		br.close();
-		bw.close();
-	}
+        int ans = 0;
+        int count = 0; // 연속된 "IOI" 개수 저장 -> 이랑 같으면  ans+1
+
+        for (int i = 1; i < M - 1; i++) {
+            if (S.charAt(i - 1) == 'I' && S.charAt(i) == 'O' && S.charAt(i + 1) == 'I') {
+                count++;
+                if (count == N) { // IOI가 N번 연속 등장
+                    ans++;
+                    count--; // 다음 IOI도 이어질 수 있으므로 하나 빼줌
+                }
+                i++; // O로 시작하는 부분 검사 불필요 -> 다음 인덱스 건너뛰자
+            } else {
+                count = 0; // 끊기면 초기화
+            }
+        }
+
+        bw.write(ans + "");
+        bw.flush();
+        br.close();
+        bw.close();
+    }
 }
