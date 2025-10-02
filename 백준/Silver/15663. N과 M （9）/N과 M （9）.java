bbb28127fc54755ba6f2625f11cd792038869912
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-10-02 15:33:56 +0900

    [Silver II] Title: N과 M (9), Time: 240 ms, Memory: 34388 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] selected, nums;
+	static boolean[] visited;
+	static Set<String> set;
+	static StringBuilder sb, ansSb;
+	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken()); // N개의 수 중
+        M = Integer.parseInt(st.nextToken()); // M개 뽑기
+        selected = new int[M];
+        nums = new int[N];
+        visited = new boolean[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+        	nums[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        ansSb = new StringBuilder();
+        set = new HashSet<>();
+        Arrays.sort(nums);
+        solve(0);
+        
+        bw.write(ansSb.toString());
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+    
+    static void solve(int sIdx) {
+    	
+    	if (sIdx == M) {
+    		sb = new StringBuilder();
+    		for (int num : selected) {
+    			sb.append(num).append(' ');
+    		}
+    		sb.append('\n');
+    		if (!set.contains(sb.toString())) {
+    			set.add(sb.toString());
+    			ansSb.append(sb.toString());
+    		} 
+    		return;
+    	}
+    	
+    	for (int i = 0; i < N; i++) {
+    		if (visited[i]) continue;
+    		
+    		selected[sIdx] = nums[i];
+    		visited[i] = true;
+    		solve(sIdx + 1);
+    		visited[i] = false;
+    	}
+    }
+}
