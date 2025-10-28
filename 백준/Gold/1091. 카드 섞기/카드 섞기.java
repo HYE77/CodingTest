f6f0f890fc0970d635099b5571b17b31c9275367
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-10-28 15:17:55 +0900

    [Gold IV] Title: 카드 섞기, Time: 212 ms, Memory: 68248 KB -BaekjoonHub

---
+++ 백준/Gold/1091. 카드 섞기/카드 섞기.java
@@ -0,0 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] P = new int[N]; // 각 카드가 누구에게 가야 하는지
+        int[] S = new int[N]; // 카드 섞는 방법
+        
+        // P 배열 채우기
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+        	P[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        // S 배열 채우기
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+        	S[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        // 초기 카드 상태
+        int[] cards = new int[N];
+        for (int i = 0; i < N; i++) {
+        	cards[i] = i % 3; // 0 1 2 0 1 2 ...
+        }
+        
+        int cnt = 0;
+        int[] temp = Arrays.copyOf(cards, N);
+        
+        while (true) {
+        	// target 배열과 같은지 확인
+        	if (Arrays.equals(temp, P)) break;
+        	
+        	// 섞기
+        	int[] tmp2 = new int[N];
+        	for (int idx = 0; idx < N; idx++) {
+        		tmp2[idx] = temp[S[idx]];
+        	}
+        	temp = Arrays.copyOf(tmp2, tmp2.length);
+        	cnt++;
+        	
+        	if (isOriginal(temp)) {
+        		cnt = -1;
+        		break;
+        	};
+        	
+        }
+        
+        System.out.println(cnt);
+        br.close();
+    }
+    
+    static boolean isOriginal(int[] temp) {
+    	boolean result = true;
+    	for (int i = 0; i < temp.length; i++) {
+    		if (temp[i] != i % 3) {
+    			result = false;
+    			break;
+    		}
+    	}
+    	
+    	return result;
+    }
+}
