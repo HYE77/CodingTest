4699e9f8be7a9852fbc3491c0bcede5cfc979cc1
Lee Hyelim <88818438+HYE77@users.noreply.github.com>
2025-11-05 14:57:21 +0900

    [Gold IV] Title: 키 순서, Time: 664 ms, Memory: 37376 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+  
+public class Main {
+      
+    static int N, M;
+    static boolean[][] dist;
+  
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+         
+        N = Integer.parseInt(st.nextToken()); // 학생 수 (정점)
+        M = Integer.parseInt(st.nextToken()); // 비교 횟수 (간선)
+             
+        dist = new boolean[N+1][N+1];
+             
+        for (int i = 0; i <= N; i++) {
+            dist[i][i] = true;
+        }
+             
+        // a -> b 연결하기
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+                 
+            dist[a][b] = true;
+        }
+        
+        // 경유해서 갈 수 있는 관계 반영하기
+        for (int k = 1; k <= N; k++) { // k: 경유지
+            for (int i = 1; i <= N; i++) { // i: 출발지
+                for (int j = 1; j <= N; j++) { // j: 도착지
+                    if (dist[i][k] && dist[k][j]) dist[i][j] = true;
+                }
+            } 
+        }
+                 
+        int[] in = new int[N+1]; // 해당 노드의 진입 차수
+        int[] out = new int[N+1]; // 해당 노드의 진출 차수
+             
+        // 각 노드로 들어올 수 있는 노드의 수와 각 노드에서 나와 도달할 수 있는 노드 수 
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                if (dist[i][j] == true) {
+                    in[j]++;
+                    out[i]++;
+                }
+            }
+        }
+             
+        // 결과 출력
+        int answer = 0;
+        for (int i = 1; i <= N; i++) {
+            if (in[i] + out[i] - 1 == N) answer++;
+        }
+ 
+        System.out.println(answer);
+        br.close();
+    }
+}
