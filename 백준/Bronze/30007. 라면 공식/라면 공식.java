2c8ce4d92c14f36f415e85c0c86cb34d7278d918
LEE Hyelim <88818438+HYE77@users.noreply.github.com>
2025-11-28 23:38:13 +0900

    [Bronze V] Title: 라면 공식, Time: 116 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+
+            System.out.println(A*(X-1)+B);
+        }
+        br.close();
+    }
+}
