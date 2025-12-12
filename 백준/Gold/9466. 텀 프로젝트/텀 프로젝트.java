4354b1fd51d6b9cbfb8d26a0af4facc3d37a7936
LEE Hyelim <88818438+HYE77@users.noreply.github.com>
2025-12-12 11:15:37 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1200 ms, Memory: 307428 KB -BaekjoonHub

---
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, tmp;
+    static int[] choice, team;
+    static List<Integer> temp;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+
+            N = Integer.parseInt(br.readLine());
+
+            choice = new int[N+1];
+//            team = new int[N+1];
+            visited = new boolean[N+1];
+
+            st = new StringTokenizer(br.readLine());
+
+            for (int i = 1; i <= N; i++) {
+                choice[i] = Integer.parseInt(st.nextToken());
+            }
+
+//            Arrays.fill(team, -1);
+//            int teamCnt = 1;
+            int ans = 0;
+            for (int i = 1; i <= N; i++) {
+                if (!visited[i]) {
+                    temp = new ArrayList<>();
+                    dfs(i, 0);
+                    ans += tmp;
+                }
+            }
+
+            System.out.println(N-ans);
+//            System.out.println(Arrays.toString(team));
+        }
+
+        br.close();
+    }
+
+    static void dfs(int idx, int count) {
+        if (visited[idx]) {
+            if (!temp.contains(idx)) {
+                tmp = 0;
+                return;
+            }
+            int from = temp.indexOf(idx);
+            tmp = count-from;
+            return;
+        }
+
+        visited[idx] = true;
+        temp.add(idx);
+        dfs(choice[idx], count+1);
+    }
+}
