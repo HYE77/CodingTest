efd3af8ec100bd7a00943557e4d31cdc59531f3e
LEE Hyelim <88818438+HYE77@users.noreply.github.com>
2025-12-15 17:49:03 +0900

    Improve code readability and structure
    Refactor input handling and add comments for clarity.

--- 백준/Gold/1633. 최고의 팀 만들기/최고의 팀 만들기.java
+++ 백준/Gold/1633. 최고의 팀 만들기/최고의 팀 만들기.java
@@ -16,6 +16,7 @@ public class Main {
 
         int cnt = 0; // 입력 인원 수
 
+        // 입력 받기
         Scanner sc = new Scanner(System.in);
         while(sc.hasNextInt()) {
             P[cnt][0] = sc.nextInt(); // 백
@@ -26,22 +27,24 @@ public class Main {
         dp[0][1][0] = P[0][0]; // 첫번째 플레이어 -> 백
         dp[0][0][1] = P[0][1]; // 첫번째 플레이어 -> 흑
 
-        int max = -1;
-        for(int i = 1; i < cnt; i++) {
-            for(int j = 0; j <= max_idx; j++) {
-                for(int k = 0; k <= max_idx; k++) {
+        int max = -1; // 능력치
+
+        for(int i = 1; i < cnt; i++) { // 전체 인원을 순회하는 변수 i
+            for(int j = 0; j <= max_idx; j++) { // 백팀에 선발된 인원 수
+                for(int k = 0; k <= max_idx; k++) { // 흑팀에 선발된 인원 수
+
                     int tmp1 = 0, tmp2 = 0;
 
-                    if (j > 0) { // i-1번째 참가자까지의 백 플레이어의 최대합 + 현재 백 플레이어값
-                        tmp1 = dp[i-1][j-1][k] + P[i][0];
-                    }
+                    // 현재 플레이어가 백팀으로 들어갈 경우
+                    if (j > 0) tmp1 = dp[i-1][j-1][k] + P[i][0];
+                    
+                    // 현재 플레이어가 흑팀으로 들어갈 경우
+                    if (k > 0) tmp2 = dp[i-1][j][k-1] + P[i][1];
 
-                    if (k > 0) {// i-1번째 참가자까지의  흑 플레이어의 최대합 + 현재 흑 플레이어값
-                        tmp2 = dp[i-1][j][k-1] + P[i][1];
-                    }
+                    // 세 가지 경우 중 가장 큰 값을 넣어줌
+                    dp[i][j][k] = Math.max(dp[i-1][j][k], Math.max(tmp1, tmp2)); 
 
-                    dp[i][j][k] = Math.max(dp[i-1][j][k], Math.max(tmp1, tmp2)); // 가장 큰 값을 넣어줌
-
+                    // 15명까지 다 비교해봤다면 최댓값 저장하기
                     if (j == max_idx && k == max_idx) {
                         max = Math.max(max, dp[i][j][k]);
                     }
