e00fb25cd16c2380ea205f6d054a914d33a17916
LEE Hyelim <88818438+HYE77@users.noreply.github.com>
2025-12-17 14:22:51 +0900

    [D2] Title: 파리 퇴치, Time: 83 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -1,44 +1,49 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		
-		for (int t = 1; t <= T; t++) {
-			int N = sc.nextInt(); // 배열 크기
-			int M = sc.nextInt(); // 파리채 크기
-			
-			int[][] arr = new int[N][N];
-			
-			for (int r = 0; r < N; r++) {
-				for (int c = 0; c < N; c++) {
-					arr[r][c] = sc.nextInt();
-				}
-			}
-			
-			int max = 0;
-			for (int i = 0; i < N-M+1; i++) {
-				for (int j = 0; j < N-M+1; j++) {
-					int sum = 0;
-					for (int x = i; x < i+M; x++) {
-						for (int y = j; y < j+M; y++) {
-							sum += arr[x][y];
-						}
-					}
-					if (sum > max) max = sum;
-				}
-			}
-			
-			System.out.println("#" + t + " " + max);
-				
-			
-		}
-		
-		sc.close();
+    static int N, M;
+    static int[][] grid, dp;
 
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            grid = new int[N][N];
+            dp = new int[N+1][N+1];
+
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    grid[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1] + grid[i-1][j-1] - dp[i-1][j-1];
+                }
+            }
+
+            int ans = -1;
+            for (int i = M; i <= N; i++) {
+                for (int j = M; j <= N; j++) {
+                    int tmp = dp[i][j] - dp[i-M][j] - dp[i][j-M] + dp[i-M][j-M];
+                    ans = Math.max(ans, tmp);
+                }
+            }
+
+            System.out.println("#" + t + " " + ans);
+        }
+
+        br.close();
+    }
 }
