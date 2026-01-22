2039698f9a17ce803710ef8ea0026d87ddaa7521
LEE Hyelim <88818438+HYE77@users.noreply.github.com>
2026-01-22 23:50:30 +0900

    [Bronze IV] Title: 2033년 밈 투표, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/29731. 2033년 밈 투표/2033년 밈 투표.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        List<String> promise = new ArrayList<>();
+        promise.add("Never gonna give you up");
+        promise.add("Never gonna let you down");
+        promise.add("Never gonna run around and desert you");
+        promise.add("Never gonna make you cry");
+        promise.add("Never gonna say goodbye");
+        promise.add("Never gonna tell a lie and hurt you");
+        promise.add("Never gonna stop");
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine().trim();
+            if (!promise.contains(input)) {
+                System.out.println("Yes");
+                return;
+            }
+        }
+        System.out.println("No");
+        br.close();
+    }
+}
