92e316e722ec27d385260284ef14d729231133a3
LEE Hyelim <88818438+HYE77@users.noreply.github.com>
2026-06-06 22:22:10 +0900

    [level 1] Title: 폰켓몬, Time: 2.75 ms, Memory: 66 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 11.1 MB, 시간: 0.80 ms
+메모리: 66 MB, 시간: 2.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 28일 20:29:21
+2026년 06월 06일 22:22:02
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        
+        int maxSize = nums.length / 2;
+        if (map.keySet().size() >= maxSize) return maxSize;
+        
+        return map.keySet().size();
+    }
+} 
